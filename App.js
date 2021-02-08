68fce8e6672743a3b9f69f84a73861fb71de7f84
kawikarob <64801223+kawikarob@users.noreply.github.com>
2021-02-08 00:59:22 -0800

    added redux

--- App.js
+++ App.js
@@ -2,15 +2,23 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
+// Redux //
+import { Provider } from "react-redux";
+import store from "./Redux/store";
+
+// Navigators //
 import Main from "./Navigators/Main";
 
+// Screens //
 import Header from "./Shared/Header";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Header />
-      <Main />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Header />
+        <Main />
+      </NavigationContainer>
+    </Provider>
   );
 }
